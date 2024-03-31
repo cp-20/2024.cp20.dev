934878ce9097136d4e3ef2e46e170c1b2a43242c
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2024-04-01 06:18:56 +0900

    `rss-parser` -> `fast-xml-parser`

--- src/servers/getArticles/trap.ts
+++ src/servers/getArticles/trap.ts
@@ -1,31 +1,38 @@
 import type { Article } from "@/servers/getArticles";
-import Parser from "rss-parser";
+import { XMLParser } from "fast-xml-parser";
+import { z } from "zod";
 
-type CustomField = {
-  "media:content": {
-    $: {
-      url: string;
-      medium: "image";
-    };
-  };
-};
-
-const parser = new Parser<unknown, CustomField>({
-  customFields: {
-    item: ["media:content"],
-  },
+const rssFeedSchema = z.object({
+  rss: z.object({
+    channel: z.object({
+      item: z.array(
+        z.object({
+          title: z.string(),
+          link: z.string(),
+          pubDate: z.string(),
+          "media:content": z.object({ "@_url": z.string() }),
+        }),
+      ),
+    }),
+  }),
 });
+
+const parser = new XMLParser({ ignoreAttributes: false });
 
 const rssUrl = "https://trap.jp/author/cp20/rss/";
 
 export const fetchTrapArticles = async () => {
-  const feed = await parser.parseURL(rssUrl);
-  const articles: Article[] = feed.items.map((item) => ({
+  const response = await fetch(rssUrl);
+  const xml = await response.text();
+  const feed = await parser.parse(xml);
+  const items = rssFeedSchema.parse(feed).rss.channel.item;
+
+  const articles: Article[] = items.map((item) => ({
     source: "trap.jp",
-    url: item.link!,
-    title: item.title!,
-    postedAt: new Date(item.pubDate!),
-    ogImageUrl: item["media:content"].$.url,
+    url: item.link,
+    title: item.title,
+    postedAt: new Date(item.pubDate),
+    ogImageUrl: item["media:content"]["@_url"],
   }));
 
   return articles;
