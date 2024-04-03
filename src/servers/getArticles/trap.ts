54740be3535c4f584054862f73aca14f57d747ab
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2024-04-03 14:49:42 +0900

    traPの記事の取得方法をRSS->URLに

--- src/servers/getArticles/trap.ts
+++ src/servers/getArticles/trap.ts
@@ -1,38 +1,57 @@
 import type { Article } from "@/servers/getArticles";
-import { XMLParser } from "fast-xml-parser";
+import jwt from "jsonwebtoken";
 import { z } from "zod";
 
-const rssFeedSchema = z.object({
-  rss: z.object({
-    channel: z.object({
-      item: z.array(
-        z.object({
-          title: z.string(),
-          link: z.string(),
-          pubDate: z.string(),
-          "media:content": z.object({ "@_url": z.string() }),
-        }),
-      ),
+const getPostsResponseSchema = z.object({
+  posts: z.array(
+    z.object({
+      title: z.string(),
+      url: z.string(),
+      published_at: z.string(),
+      feature_image: z.string().nullable(),
     }),
-  }),
+  ),
 });
 
-const parser = new XMLParser({ ignoreAttributes: false });
+const key = process.env.GHOST_ADMIN_API_KEY;
+if (!key) throw new Error("GHOST_ADMIN_API_KEY is not set");
+const [id, secret] = key.split(":");
 
-const rssUrl = "https://trap.jp/author/cp20/rss/";
+const searchParams = new URLSearchParams({
+  includes: "authors",
+  fields: "title,url,feature_image,published_at",
+  filter: "authors.name:cp20+status:published+visibility:public",
+  limit: "all",
+  order: "published_at desc",
+});
+const apiUrl = new URL("https://blog-admin.trap.jp/ghost/api/admin/posts");
+apiUrl.search = searchParams.toString();
+
+const getPosts = async () => {
+  const token = jwt.sign({}, Buffer.from(secret, "hex"), {
+    keyid: id,
+    algorithm: "HS256",
+    expiresIn: "5m",
+    audience: `/admin/`,
+  });
+
+  const headers = { Authorization: `Ghost ${token}` };
+  const res = await fetch(apiUrl, { headers });
+  const json = await res.json();
+  const data = getPostsResponseSchema.parse(json);
+
+  return data.posts;
+};
 
 export const fetchTrapArticles = async () => {
-  const response = await fetch(rssUrl);
-  const xml = await response.text();
-  const feed = await parser.parse(xml);
-  const items = rssFeedSchema.parse(feed).rss.channel.item;
+  const posts = await getPosts();
 
-  const articles: Article[] = items.map((item) => ({
+  const articles: Article[] = posts.map((post) => ({
     source: "trap.jp",
-    url: item.link,
-    title: item.title,
-    postedAt: new Date(item.pubDate),
-    ogImageUrl: item["media:content"]["@_url"],
+    url: post.url,
+    title: post.title,
+    postedAt: new Date(post.published_at),
+    ogImageUrl: post.feature_image ?? undefined,
   }));
 
   return articles;
