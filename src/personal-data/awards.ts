ffe7d30fa06ad5a5d08014786f4bc420b3e84487
cp-20 <n.naoki.1587@gmail.com>
2024-12-01 22:10:25 +0900

    🍖 🥟 🥭 MIXI Bug Shooting Challenge #11 の実績を足した

--- src/personal-data/awards.ts
+++ src/personal-data/awards.ts
@@ -6,6 +6,12 @@ export type Award = {
 
 export const awards = [
   {
+    label: "MIXI Bug Shooting Challenge 優勝",
+    date: "2024/12",
+    url:
+      "https://mixi-recruit.snar.jp/jobboard/detail.aspx?id=-fwHdaAAwGZ-bG-AvP9r2A",
+  },
+  {
     label: "Web Speed Hackathon 2024 優勝",
     date: "2024/03",
     url: "https://github.com/CyberAgentHack/web-speed-hackathon-2024",
@@ -23,7 +29,8 @@ export const awards = [
   {
     label: "第40回千葉市教育・文化・スポーツ等功労者賞",
     date: "2023/03",
-    url: "https://www.city.chiba.jp/somu/shichokoshitsu/hisho/40kyouikubunkasports.html",
+    url:
+      "https://www.city.chiba.jp/somu/shichokoshitsu/hisho/40kyouikubunkasports.html",
   },
   {
     label: "MAKERS UNIVERSITY U-18 8期生 採択",
@@ -33,7 +40,8 @@ export const awards = [
   {
     label: "Microsoft Imagine Cup 2022 アジア教育部門優勝",
     date: "2022/03",
-    url: "https://techcommunity.microsoft.com/t5/student-developer-blog/meet-the-top-3-teams-of-the-2022-imagine-cup/ba-p/3274948",
+    url:
+      "https://techcommunity.microsoft.com/t5/student-developer-blog/meet-the-top-3-teams-of-the-2022-imagine-cup/ba-p/3274948",
   },
   {
     label: "第19回高校生高専生科学技術チャレンジ (JSEC) 優秀賞",
